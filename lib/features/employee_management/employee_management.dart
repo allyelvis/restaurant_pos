ae1d91b294ae40cf739cd26cb1929545e4b24d80
allyelvis <allyelvis6569@gmail.com>
2024-10-09 08:58:47 +0200

    Initial commit for Restaurant POS Flutter app

---
+++ lib/features/employee_management/employee_management.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmployeeManagement extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Employee Management'),
+      ),
+      body: Center(
+        child: Text('Manage employees here'),
+      ),
+    );
+  }
+}
---
+++ lib/features/inventory_management/inventory_management.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InventoryManagement extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inventory Management'),
+      ),
+      body: Center(
+        child: Text('Manage inventory here'),
+      ),
+    );
+  }
+}
